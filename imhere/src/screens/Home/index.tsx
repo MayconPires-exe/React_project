9e487ef5de0854f9220b9ffffd7fc25330607401
MayconPires <maycon.pires@tmh.com.br>
2023-01-18 17:59:27 -0300

    Create a project event

---
+++ imhere/src/screens/Home/index.tsx
@@ -0,0 +1,37 @@
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+
+import { styles } from "./styles";
+
+export function Home() {
+
+  function handleParticipantAdd() {
+    console.log('Você acabou de adicionar um novo participante!');
+  }
+
+  return(
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Terça, 17 de Janeiro de 2023
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input} 
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+    </View>
+  );
+}
